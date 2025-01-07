d8cb64916caea3d65e4770580a7f81a7695c3217
sterlingking1990 <izundukingsleyemeka@gmail.com>
2025-01-07 08:36:16 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: false, // Ensures that errors are not ignored during build
-  },
 };
 
 export default nextConfig;
